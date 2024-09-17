d95ef3bd0b49708bee5cda5cea542abd8a29f13f
Allan Araujo Lima <62760029+Allan-Araujo-Lima@users.noreply.github.com>
2024-09-17 20:12:07 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: configService.get('FRONTEND_URL'),
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['content-type', 'authorization'],
   });
 
   app.useGlobalPipes(
