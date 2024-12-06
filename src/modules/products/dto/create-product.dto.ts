1d9adfe60c9a8bcdc3183494bbfe7be98f037b0a
Allan Araújo lima <allanvoide@gmail.com>
2024-12-05 22:26:53 -0300

    fix: fixing bug that database wasn't accepiting price and amount from request

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,16 +1,14 @@
-import { IsDateString, IsInt, IsNotEmpty, IsNumber, IsPositive } from "class-validator";
+import { IsDateString, IsInt, IsNotEmpty, IsNumber, IsNumberString, IsPositive } from "class-validator";
 
 export class CreateProductDto {
     @IsNotEmpty()
     name: string;
     description: string;
     @IsNotEmpty()
-    // @IsNumber({ allowInfinity: false, allowNaN: false })
-    // @IsPositive()
+    @IsNumberString()
     price: number;
     @IsNotEmpty()
-    // @IsPositive()
-    // @IsInt()
+    @IsNumberString()
     amount: number;
     @IsDateString()
     expiration: string
